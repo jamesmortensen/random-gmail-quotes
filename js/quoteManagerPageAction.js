9ead10e6d385007c3fb01175f68c604c24d8656b
James Mortensen <jem@Jamess-MacBook-Pro.local>
2013-09-18 21:26:51 -0700

    Modified the page action so that one could paste a quote as a string without putting it in an array, for situations where there's only one quote.

--- js/quoteManagerPageAction.js
+++ js/quoteManagerPageAction.js
@@ -24,7 +24,11 @@ window.addEventListener("load", function() {
 	
 	$('[data-action="save"]').click(function() {
 		var items = {};
-		items["m_quotes"] = JSON.parse( $('[data-value="quotes"]').val() );
+		if($('[data-value="quotes"]').val().match(/\[\".*/g) != null) {
+		    items["m_quotes"] = JSON.parse( $('[data-value="quotes"]').val() );
+		} else {
+			items["m_quotes"] = [$('[data-value="quotes"]').val()];
+		}
 		//items["m_quotes"] = ["<img src=\"http://se-flair.appspot.com/png/921455be-18e4-481e-ba1a-ba1d30998061/\" />","James was here!!!!!!"];
 		chrome.storage.local.set(items);
 		//chrome.tabs.executeScript(null,{code:"document.body.dataset['quote'] = 'fasfdtest';document.body.style.backgroundColor='orange';"});
